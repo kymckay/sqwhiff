a6d1461e003307c57145ecd1f73628f23cb628ec
Kyle Mckay <kymckay.dev@gmail.com>
2021-04-25 00:41:06 +0100

    Use default parameter values in Token constructor
    Cleaner method of allowing use with less arguments

--- src/token.h
+++ src/token.h
@@ -12,24 +12,11 @@ enum class TokenType {
 };
 
 struct Token {
-    TokenType type = TokenType::unknown;
+    TokenType type;
     std::string raw;
-    int line; // Line and column of first character in overall text
+    int line; // Line of first character in file
 
-    Token() {}
-
-    Token(std::string raw, int line)
-    {
-        this->raw = raw;
-        this->line = line;
-    }
-
-    Token(TokenType type, std::string raw, int line)
-    {
-        this->type = type;
-        this->raw = raw;
-        this->line = line;
-    }
+    Token(TokenType type = TokenType::unknown, std::string raw = "", int line = -1) : type(type), raw(raw), line(line) {}
 };
 
 // Using a typedef prevents naming clashes in global name space
