81e37a90c57e8cf6dbfe94d003b21ddaf3a1f37f
Kyle Mckay <kymckay.dev@gmail.com>
2021-04-25 13:54:06 +0100

    Update main method to use the parser
    Also committing the .vscode configuration files to control build setup

--- src/sqf.cpp
+++ src/sqf.cpp
@@ -7,29 +7,8 @@
 int main()
 {
     std::ifstream file_in("test.txt");
-    Lexer test(file_in);
-    Token t;
-    do
-    {
-        t = test.nextToken();
-        switch (t.type)
-        {
-        case TokenType::plus:
-            std::cout << "plus ";
-            break;
-        case TokenType::minus:
-            std::cout << "minus ";
-            break;
-        case TokenType::number:
-            std::cout << "number ";
-            break;
-        case TokenType::end_of_file:
-            std::cout << "eof ";
-            break;
-        default:
-            std::cout << "unknown ";
-            break;
-        }
-        std::cout << t.line << ": " << t.raw << "\n";
-    } while (t.type != TokenType::end_of_file);
+    Lexer lex(file_in);
+    Parser par(lex);
+
+    std::cout << par.expr();
 }
