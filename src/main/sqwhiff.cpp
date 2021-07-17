a79edc3b7157a17b6ad3a86d7a8d9db9b3fcf84d
Kyle Mckay <kymckay.dev@gmail.com>
2021-07-17 19:21:28 +0100

    Use command line arguments to specify file inputs
    Currently just takes file paths, relative to execution environment
    or full paths should both work.
    
    Need to add tests for the main binary.
    
    Closes #17

--- src/main/sqwhiff.cpp
+++ src/main/sqwhiff.cpp
@@ -4,22 +4,58 @@
 #include "src/analyzer/analyzer.h"
 #include <iostream>
 #include <fstream>
+#include <string>
 
-int main()
+// TODO: Add way to input a directory to fully analyze
+static void show_usage()
 {
-    std::ifstream file_in("test.txt");
+    std::cerr << "Usage: sqwhiff <option(s)> FILE_PATHS\n"
+              << "Options:\n"
+              << "\t-h,--help\t\tShow this help message and exit\n"
+              << std::endl;
+}
 
-    if (file_in.is_open())
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
     {
-        Preprocessor preproc(file_in);
-        Lexer lex(preproc);
-        Parser p(lex);
-        Analyzer a(p, std::cout);
-        a.analyze();
-        file_in.close();
+        show_usage();
+        return 1;
     }
-    else
+
+    std::string p1(argv[1]);
+    if (p1 == "-h" || p1 == "--help")
     {
-        std::cout << "Unable to open file";
+        show_usage();
+        return 0;
     }
+
+    int errorc = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        std::ifstream file_in(argv[i]);
+
+        // TODO: Sum up errors encountered
+        if (file_in.is_open())
+        {
+            Preprocessor preproc(file_in);
+            Lexer lex(preproc);
+            Parser p(lex);
+            Analyzer a(p, std::cout);
+            a.analyze();
+            file_in.close();
+        }
+        else
+        {
+            std::cerr << "Unable to open file: " << argv[i];
+            errorc++; // Bad usage should count as an error
+        }
+    }
+
+    if (errorc == 0)
+    {
+        std::cout << "No errors found!" << std::endl;
+    }
+
+    return errorc;
 }
