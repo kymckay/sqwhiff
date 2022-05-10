a4358c171e9710845418f90edf2bcd276ed02f06
Kyle Mckay <kymckay.dev@gmail.com>
2022-05-10 12:11:43 +0100

    test(rules): ensure correct return types analysed
    - Analysis is mistakenly using values in arrays as the expression return.
    - Want to ensure commands which can have differing returns are correctly
      selected based on the parameter types used.

--- test/rules/type_test.cpp
+++ test/rules/type_test.cpp
@@ -39,7 +39,28 @@ TEST_F(AnalyzerTest, ReportsIncorrectTypeInArray) {
   EXPECT_EQ("1:4 SemanticError - Incorrect type used with: round\n", output_);
 }
 
-TEST_F(AnalyzerTest, DISABLED_ReportsIncorrecTypeFromVariable) {
+TEST_F(AnalyzerTest, HandlesArrays) {
+  analyze("[21,5,3] select [0,1];", type_rule);
+  EXPECT_EQ(0, errorc_);
+  EXPECT_EQ("", output_) << "The expressions inside the arrays should not "
+                            "confuse analysis of `select`.";
+}
+
+TEST_F(AnalyzerTest, HandlesDifferingParameters) {
+  analyze("[] select []; \"\" select []", type_rule);
+  EXPECT_EQ(0, errorc_);
+  EXPECT_EQ("", output_) << "There are multiple ways to use `select`, any "
+                            "valid permutation should work.";
+}
+
+TEST_F(AnalyzerTest, ReportsIncorrectDifferingReturn) {
+  analyze("comment (\"\" select [0,2]); comment ([] select [0,2])", type_rule);
+  EXPECT_EQ(1, errorc_);
+  EXPECT_EQ("1:28 SemanticError - Incorrect type used with: comment\n", output_)
+      << "The return type of `select` should differ by usage";
+}
+
+TEST_F(AnalyzerTest, DISABLED_ReportsIncorrectTypeFromVariable) {
   analyze("_test = 1; allturrets _test", type_rule);
   EXPECT_EQ(1, errorc_);
   EXPECT_EQ("1:11 SemanticError - Incorrect type used with: allturrets\n",
